d98e9bf90e62351c1d76aff5db43829e8b92d342
BoffinBee <boffinbee@gmail.com>
2022-10-26 13:20:26 +0100

    refactored the server.js file

--- server.js
+++ server.js
@@ -3,32 +3,40 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer(function (req, res) {
+    const notFound = path.join(__dirname, 'public', 'notFound.html')
+    let filePath = notFound
     if (req.url === '/' || req.url == '/index.html') {
-        let filePath = path.join(__dirname, 'public', 'index.html')
-        fs.readFile(filePath, 'utf8', function (err, data) {
-            if (err) throw err
-            res.writeHead(200, {'content-type': 'text/html'})
-            res.end(data)
-        })
+        filePath = path.join(__dirname, 'public', 'index.html')
+    } else if (req.url === '/contact.html') {
+        filePath = path.join(__dirname, 'public', 'contact.html')
+    } else if (req.url === '/about.html') {
+        filePath = path.join(__dirname, 'public', 'about.html')
     }
 
-    if (req.url === '/contact.html') {
-        let filePath = path.join(__dirname, 'public', 'contact.html')
-        fs.readFile(filePath, 'utf8', function (err, data) {
-            if (err) throw err
-            res.writeHead(200, {'content-type': 'text/html'})
-            res.end(data)
-        })
-    }
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) throw err
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.end(data)
+    })
 
-    if (req.url === '/about.html') {
-        let filePath = path.join(__dirname, 'public', 'about.html')
-        fs.readFile(filePath, 'utf8', function (err, data) {
-            if (err) throw err
-            res.writeHead(200, {'content-type': 'text/html'})
-            res.end(data)
-        })
-    }
+
+    // if (req.url === '/contact.html') {
+    //     let filePath = path.join(__dirname, 'public', 'contact.html')
+    //     fs.readFile(filePath, 'utf8', function (err, data) {
+    //         if (err) throw err
+    //         res.writeHead(200, {'content-type': 'text/html'})
+    //         res.end(data)
+    //     })
+    // }
+
+    // if (req.url === '/about.html') {
+    //     let filePath = path.join(__dirname, 'public', 'about.html')
+    //     fs.readFile(filePath, 'utf8', function (err, data) {
+    //         if (err) throw err
+    //         res.writeHead(200, {'content-type': 'text/html'})
+    //         res.end(data)
+    //     })
+    // }
 
 })
 
