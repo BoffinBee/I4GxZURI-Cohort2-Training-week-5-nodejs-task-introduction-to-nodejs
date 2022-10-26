8f55cd7569e14c562a2bdee06c41bb33264099ff
BoffinBee <boffinbee@gmail.com>
2022-10-26 13:34:00 +0100

    cleaned up the server file

--- server.js
+++ server.js
@@ -18,26 +18,6 @@ const server = http.createServer(function (req, res) {
         res.writeHead(200, {'content-type': 'text/html'})
         res.end(data)
     })
-
-
-    // if (req.url === '/contact.html') {
-    //     let filePath = path.join(__dirname, 'public', 'contact.html')
-    //     fs.readFile(filePath, 'utf8', function (err, data) {
-    //         if (err) throw err
-    //         res.writeHead(200, {'content-type': 'text/html'})
-    //         res.end(data)
-    //     })
-    // }
-
-    // if (req.url === '/about.html') {
-    //     let filePath = path.join(__dirname, 'public', 'about.html')
-    //     fs.readFile(filePath, 'utf8', function (err, data) {
-    //         if (err) throw err
-    //         res.writeHead(200, {'content-type': 'text/html'})
-    //         res.end(data)
-    //     })
-    // }
-
 })
 
 const port = 2040
