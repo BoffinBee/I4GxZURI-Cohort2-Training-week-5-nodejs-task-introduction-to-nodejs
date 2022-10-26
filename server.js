93638021abd854e420339af5ff2e3011ea6a28f3
BoffinBee <boffinbee@gmail.com>
2022-10-26 13:01:58 +0100

    modified server to run html files

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.end('IF YOU SEE ME, SERVER IS UP!')
+    if (req.url === '/' || req.url == '/index.html') {
+        let filePath = path.join(__dirname, 'public', 'index.html')
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            if (err) throw err
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.end(data)
+        })
+    }
+
+    if (req.url === '/contact.html') {
+        let filePath = path.join(__dirname, 'public', 'contact.html')
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            if (err) throw err
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.end(data)
+        })
+    }
+
+    if (req.url === '/about.html') {
+        let filePath = path.join(__dirname, 'public', 'about.html')
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            if (err) throw err
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.end(data)
+        })
+    }
+
 })
 
-server.listen(2040, '127.0.0.1')
+const port = 2040
+const ip_address = '127.0.0.1'
 
-console.log("Server is up and running!")
+server.listen(port, ip_address)
+
+console.log(`Server is up and running on ${ip_address}:${port}`)
